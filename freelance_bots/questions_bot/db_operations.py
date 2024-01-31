7942e4b198df9587c6fca571e70530df2c18fd0e
balu83vs <balu83vs@gmail.com>
2024-01-31 08:25:57 +0300

    FSM - add question - DONE

--- freelance_bots/questions_bot/db_operations.py
+++ freelance_bots/questions_bot/db_operations.py
@@ -1,5 +1,7 @@
 from db_create import db_connect
 
+from datetime import datetime
+
 # функция первоначального внесения пользователей в базу данных
 def new_user_creating(user_id, company_id = 0, team_id = 0, admin = 0):
     # Подключение к базе данных SQLite
@@ -12,7 +14,7 @@ def new_user_creating(user_id, company_id = 0, team_id = 0, admin = 0):
         conn.close()
         return False
     else:
-        # внесение записи о пользователе в базу 
+        # внесение записи о пользователе в базу users 
         cursor.execute('''
             INSERT INTO users (user_id, company_id, team_id, admin)
             VALUES (?, ?, ?, ?)
@@ -21,6 +23,16 @@ def new_user_creating(user_id, company_id = 0, team_id = 0, admin = 0):
         conn.commit()
         conn.close()
         return True
+
+# проверка административных прав
+def check_admin_permissions(user_id):
+    # Подключение к базе данных SQLite
+    conn, cursor = db_connect()
+    sql = "select * from users where user_id=:user_id and admin=:admin"
+    cursor.execute(sql, {"user_id": user_id, "admin": 1})
+    admin = cursor.fetchall()
+    conn.close()
+    return admin        
 
 # функция для получения списка пользователей из базы данных
 def get_users_from_database():
@@ -45,15 +57,30 @@ def get_current_question_id():
 def get_next_question():
     pass
 
-# функция сохранения ответа в БД
-def save_answer(user_id, question_id, answer):
-
+# функция сохранения вопроса в БД
+def save_question(data):
     # Подключение к базе данных SQLite
     conn, cursor = db_connect()
-
-    # 
+    date = datetime.now()
+    # внесение записи о вопросе в базу questions
     cursor.execute('''
-        INSERT OR REPLACE INTO answers (user_id, question_id, answer)
+        INSERT OR REPLACE INTO questions (title, date, team_id, type)
+        VALUES (?, ?, ?, ?)
+    ''', (data.get('question'), date, data.get('team_id'), data.get('type')))
+    try: 
+        conn.commit()
+        return True
+    except:
+        return False
+
+# функция сохранения ответа в БД
+def save_answer(question_id, answer):
+    # Подключение к базе данных SQLite
+    conn, cursor = db_connect()
+    date = datetime.now()
+    # внесение записи об ответе в базу questions
+    cursor.execute('''
+        INSERT OR REPLACE INTO answers (question_id, date, answer)
         VALUES (?, ?, ?)
-    ''', (user_id, question_id, answer))
+    ''', (question_id, date, answer))
     conn.commit()
--- freelance_bots/questions_bot/main.py
+++ freelance_bots/questions_bot/main.py
@@ -4,24 +4,40 @@ import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters.command import Command
 
+from aiogram.filters.state import State, StatesGroup, StateFilter
+from aiogram.fsm.context import FSMContext
+
 from config import TOKEN
 
 from db_create import db_create
 from db_operations import (
-                       new_user_creating, get_users_from_database,
+                       new_user_creating, check_admin_permissions, get_users_from_database,
                        get_current_question_id, get_next_question, get_question_from_database,
-                       save_answer)
+                       save_answer, save_question)
 
+help = """
+Справочная информация о программе
+будет заполнена позже.
+"""
 
 # Настройка логирования
 logging.basicConfig(level=logging.INFO)
 
 # Инициализация бота и диспетчера
 bot = Bot(token=TOKEN)
-dp = Dispatcher(bot = bot)
+dp = Dispatcher()
 
-# Создание объекта БД
 db_create()
+
+# стартовая функция бота
+async def on_startup(_):                                                               
+    await print('Бот - On-line')
+    await db_create()
+
+# Обработчик команды /help
+@dp.message(Command("help"))
+async def start(message: types.Message):
+    await message.answer(help)
 
 # Обработчик команды /start
 @dp.message(Command("start"))
@@ -32,19 +48,68 @@ async def start(message: types.Message):
     else:
         await message.answer("Вы уже есть в Базе.")
 
-# Обработчик команды /send
-@dp.message(Command("send"))
-async def send_questions(message: types.Message):
-    # Получаем список пользователей
-    users = get_users_from_database()  # список пользователей из базы данных
-    # Отправляем вопросы каждому пользователю
-    for user_id in users:
-        #question = get_question_from_database()  # вопрос из базы данных
-        #buttons = [types.KeyboardButton(answer) for answer in question['answers']]
-        #keyboard_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        #keyboard_markup.add(*buttons)
-        #await bot.send_message(user_id, question['text'], reply_markup=keyboard_markup)
-        await bot.send_message(user_id[0], f'Проверка {user_id[0]}')
+
+###################################### начало блока машины состояний #################################################
+class FSMAdmin(StatesGroup):
+    team_id = State()
+    question = State()
+    type = State()        
+
+# Обработчик команды /sendall (запуск машины состояний)
+@dp.message(StateFilter(None), Command("sendall"))
+async def enter_team_id(message: types.Message, state: FSMContext):
+    user_id = message.from_user.id
+    # проверка прав доступа
+    if not check_admin_permissions(user_id):
+        await message.answer(f"Пользователь {user_id} не является администратором")
+    else: 
+        await message.answer('Введите айди команды, для которой отправить вопрос.')
+        await state.set_state(FSMAdmin.team_id)
+
+# Обработчик команды выхода из машины состояний
+@dp.message(Command("exit"))
+async def cancel_handler(message: types.Message, state: FSMContext):
+    user_id = message.from_user.id
+    if check_admin_permissions(user_id):             
+        curent_state = await state.get_state()                            
+        if curent_state is None:                                         
+            return
+        await state.finish()                                                
+        await message.reply('OK')     
+
+# функция загрузки team_id в словарь
+@dp.message(FSMAdmin.team_id)                                 
+async def enter_question(message : types.Message, state: FSMContext):      
+    user_id = message.from_user.id
+    if check_admin_permissions(user_id):     
+        await state.update_data(team_id=message.text)                                  
+        await message.answer('Введите вопрос.')
+        await state.set_state(FSMAdmin.question)                     
+
+# функция загрузки question в словарь
+@dp.message(FSMAdmin.question)                                 
+async def enter_type(message : types.Message, state: FSMContext):      
+    user_id = message.from_user.id
+    if check_admin_permissions(user_id):                                       
+        await state.update_data(question=message.text)                                  
+        await message.answer('Введите тип вопроса.')
+        await state.set_state(FSMAdmin.type)  
+
+# функция загрузки type в словарь
+@dp.message(FSMAdmin.type)                                 
+async def enter_type(message : types.Message, state: FSMContext):      
+    user_id = message.from_user.id
+    if check_admin_permissions(user_id):                                       
+        await state.update_data(type=message.text)   
+        data = await state.get_data()                              
+        if save_question(data):
+            await state.clear()
+            await message.answer('Вопрос успешно сохранен в базе.')
+        else:
+            await message.answer('Ошибка при сохранении вопроса.')    
+
+###################################### завершение блока машины состояний #################################################
+
 
 # Обработчик ответов на вопросы
 @dp.message()
@@ -69,7 +134,7 @@ async def handle_answer(message: types.Message):
 
 # Запуск процесса поллинга новых апдейтов
 async def main():
-    await dp.start_polling(bot)
+    await dp.start_polling(bot, on_startup=on_startup)
 
 if __name__ == "__main__":
     asyncio.run(main())    
